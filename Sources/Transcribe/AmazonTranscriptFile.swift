1ef6f6cf1ed2aca0607eac3afaf145b3e7633071
Ole Begemann <ole@oleb.net>
2019-01-23 19:31:25 +0100

    Rename files

---
+++ Sources/Transcribe/AmazonTranscriptFile.swift
@@ -0,0 +1,66 @@
+import Foundation
+
+extension AmazonTranscribe {
+    /// A 1-to-1 reproduction of the format of an Amazon Transcribe transcription result JSON file.
+    /// The aim is to map the file format as-is, not to make it as Swift-friendly as possible.
+    /// Field names and types follow the original JSON structure.
+    public struct TranscriptFile: Decodable {
+        var jobName: String
+        var accountId: String
+        var results: Results
+
+        struct Results: Decodable {
+            var transcripts: [Transcript]
+            var speaker_labels: SpeakerLabels
+            var items: [Item]
+        }
+
+        struct Transcript: Decodable {
+            var transcript: String
+        }
+
+        struct SpeakerLabels: Decodable {
+            var speakers: Int
+            var segments: [SpeakerSegment]
+        }
+
+        struct SpeakerSegment: Decodable {
+            var speaker_label: String
+            var start_time: String
+            var end_time: String
+            var items: [Item]
+
+            struct Item: Decodable {
+                var speaker_label: String
+                var start_time: String
+                var end_time: String
+            }
+        }
+
+        struct Item: Decodable {
+            var start_time: String?
+            var end_time: String?
+            var alternatives: [Alternative]
+            var type: Kind
+
+            struct Alternative: Decodable {
+                var content: String
+                var confidence: String?
+            }
+
+            enum Kind: String, Decodable {
+                case pronunciation
+                case punctuation
+            }
+        }
+    }
+}
+
+extension AmazonTranscribe.TranscriptFile {
+    /// Loads and parses an Amazon Transcribe transcription result JSON file.
+    public init(file: URL) throws {
+        let jsonData = try Data(contentsOf: file)
+        let decoder = JSONDecoder()
+        self = try decoder.decode(AmazonTranscribe.TranscriptFile.self, from: jsonData)
+    }
+}
---
+++ Tests/TranscribeTests/AmazonTranscriptFileTests.swift
@@ -0,0 +1,22 @@
+import XCTest
+@testable import Transcribe
+
+final class AmazonTranscriptFileTests: XCTestCase {
+    func test_parseAmazonTranscribeJSONFile() {
+        let file = Bundle(for: type(of: self)).url(forResource: "amazon-transcribe-swift-community-podcast-0001", withExtension: "json")!
+        do {
+            let sut = try AmazonTranscribe.TranscriptFile(file: file)
+            XCTAssertEqual(sut.jobName, "swift-community-podcast-0001")
+            XCTAssertEqual(sut.accountId, "***REMOVED***")
+            XCTAssertEqual(sut.results.speaker_labels.segments.count, 324)
+            XCTAssertEqual(sut.results.speaker_labels.speakers, 4)
+            XCTAssertEqual(sut.results.items.count, 15204)
+        } catch {
+            XCTFail(String(reflecting: error))
+        }
+    }
+
+    static var allTests = [
+        ("test_parseAmazonTranscribeJSONFile", test_parseAmazonTranscribeJSONFile),
+    ]
+}
