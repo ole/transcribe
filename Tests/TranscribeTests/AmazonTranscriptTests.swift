5f01b8bde63b9e23d7562d78d949f65206cf638a
Julian Kahnert <mail@juliankahnert.de>
2019-02-05 16:50:50 +0100

    cut transcript and add another unit test

--- Tests/TranscribeTests/AmazonTranscriptTests.swift
+++ Tests/TranscribeTests/AmazonTranscriptTests.swift
@@ -22,6 +22,27 @@ final class AmazonTranscriptTests: XCTestCase {
             XCTFail(String(reflecting: error))
         }
     }
+    
+    func test_makeTranscriptFromFileShort() {
+        do {
+            let inputFile = try fixturesDirectory().appendingPathComponent("amazon-transcribe-swift-community-podcast-0001-formatted-short.json")
+            let sut = try AmazonTranscribe.Transcript(file: inputFile)
+            XCTAssertEqual(sut.segments.count, 2)
+            XCTAssertEqual(sut.speakers.count, 2)
+            let firstSegmentFragments = sut.segments[0].fragments
+            let pronunciationFragments = firstSegmentFragments.filter { fragment in
+                if case .pronunciation = fragment.kind { return true } else { return false }
+            }
+            let punctuationFragments = firstSegmentFragments.filter { fragment in
+                if case .punctuation = fragment.kind { return true } else { return false }
+            }
+            XCTAssertEqual(firstSegmentFragments.count, 69)
+            XCTAssertEqual(pronunciationFragments.count, 59)
+            XCTAssertEqual(punctuationFragments.count, 10)
+        } catch {
+            XCTFail(String(reflecting: error))
+        }
+    }
 
     static var allTests = [
         ("test_makeTranscriptFromFile", test_makeTranscriptFromFile),
