f986e5e2b59a1d74bbc5632d6f9ca928b287e92b
Ole Begemann <ole@oleb.net>
2019-02-10 18:49:06 +0100

    Update Linux test manifest

--- Tests/TranscribeTests/XCTestManifests.swift
+++ Tests/TranscribeTests/XCTestManifests.swift
@@ -2,13 +2,24 @@ import XCTest
 
 extension AmazonRawTranscriptTests {
     static let __allTests = [
-        ("test_parseAmazonTranscribeJSONFile", test_parseAmazonTranscribeJSONFile)
+        ("test_parseAmazonTranscribeJSONFile", test_parseAmazonTranscribeJSONFile),
     ]
 }
 
 extension AmazonTranscriptTests {
     static let __allTests = [
-        ("test_makeTranscriptFromFile", test_makeTranscriptFromFile)
+        ("test_fragmentText_pronunciation", test_fragmentText_pronunciation),
+        ("test_fragmentText_punctuation", test_fragmentText_punctuation),
+        ("test_makeTranscriptFromFile", test_makeTranscriptFromFile),
+        ("test_makeTranscriptFromFileShort", test_makeTranscriptFromFileShort),
+        ("test_segmentText", test_segmentText),
+        ("test_segmentText_trailingPronunciationShouldNotHaveTrailingSpace", test_segmentText_trailingPronunciationShouldNotHaveTrailingSpace),
+    ]
+}
+
+extension WebVTTParserTest {
+    static let __allTests = [
+        ("test_parseAWSTranscript", test_parseAWSTranscript),
     ]
 }
 
@@ -16,7 +27,8 @@ extension AmazonTranscriptTests {
 public func __allTests() -> [XCTestCaseEntry] {
     return [
         testCase(AmazonRawTranscriptTests.__allTests),
-        testCase(AmazonTranscriptTests.__allTests)
+        testCase(AmazonTranscriptTests.__allTests),
+        testCase(WebVTTParserTest.__allTests),
     ]
 }
 #endif
