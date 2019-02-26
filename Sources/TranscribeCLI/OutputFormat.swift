8cf0e085c057b24a8d6178522480f9d8b875061f
Ole Begemann <ole@oleb.net>
2019-02-26 22:33:03 +0100

    Small refactorings and text changes for CLI tool

--- Sources/TranscribeCLI/OutputFormat.swift
+++ Sources/TranscribeCLI/OutputFormat.swift
@@ -1,15 +1,15 @@
 import Utility
 
 enum OutputFormat: String, CaseIterable {
-    case markdown
-    case webvtt
+    case markdown = "markdown"
+    case webVTT = "webvtt"
 
-    static let `default` = webvtt
+    static let `default` = webVTT
 }
 
 extension OutputFormat: StringEnumArgument {
     static let completion: ShellCompletion = .values([
         (markdown.rawValue, "Markdown output format"),
-        (webvtt.rawValue, "WebVTT output format")
+        (webVTT.rawValue, "WebVTT output format")
     ])
 }
--- Sources/TranscribeCLI/main.swift
+++ Sources/TranscribeCLI/main.swift
@@ -18,39 +18,38 @@ import Utility
 // MARK: - Main program
 
 do {
-    // setup the argument parser
+    // Set up the argument parser
     let parser = ArgumentParser(usage: "<options>",
                                 overview: "A Swift parser for output files from automated transcription services.")
-    let rawInputFilename = parser.add(option: "--json", shortName: "-j", kind: PathArgument.self, usage: "Amazon transcribe json file, e.g. './TestFixtures/amazon-transcribe-swift-community-podcast-0001-formatted-short.json'")
-    let rawSpeakerNames = parser.add(option: "--names", shortName: "-n", kind: [String].self, usage: "Array of speaker names, e.g. 'alice bob'")
-    let possibleOutputFormats = OutputFormat.allCases.map({ $0.rawValue }).joined(separator: ", ")
-    let outputFormatArgument = parser.add(option: "--format", shortName: "-f", kind: OutputFormat.self, usage: "Specify the output format. Possible values are \(possibleOutputFormats). If omitted, \(OutputFormat.default) will be used.", completion: OutputFormat.completion)
-    let outputArgument = parser.add(option: "--output", shortName: "-o", kind: PathArgument.self, usage: "Specify the output file path. If omitted, output will be written into the standard output.")
+    let inputFileArgument = parser.add(option: "--json", shortName: "-j", kind: PathArgument.self, usage: "Amazon Transcribe JSON file, e.g. './TestFixtures/amazon-transcribe-swift-community-podcast-0001.json'")
+    let speakerNamesArgument = parser.add(option: "--names", shortName: "-n", kind: [String].self, usage: "Array of speaker names, e.g. 'alice bob'")
+    let supportedOutputFormats = OutputFormat.allCases.map({ $0.rawValue }).joined(separator: ", ")
+    let outputFormatArgument = parser.add(option: "--format", shortName: "-f", kind: OutputFormat.self, usage: "Specify the output format. Supported formats: \(supportedOutputFormats). The default is \(OutputFormat.default).", completion: OutputFormat.completion)
+    let outputFileArgument = parser.add(option: "--output", shortName: "-o", kind: PathArgument.self, usage: "Output file path. If omitted, output will be written to the standard output.")
     
     // The first argument is always the executable, drop it
     let arguments = Array(ProcessInfo.processInfo.arguments.dropFirst())
     let parsedArguments = try parser.parse(arguments)
-    guard let inputFilename = parsedArguments.get(rawInputFilename),
+    guard let inputFilename = parsedArguments.get(inputFileArgument),
         inputFilename.path.asString.lowercased().hasSuffix("json") else {
         throw ArgumentParserError.invalidValue(argument: "--json", error: ArgumentConversionError.custom("No json file specified"))
     }
    
-    // validate json filename
+    // Validate input file
     let inputFile = URL(fileURLWithPath: inputFilename.path.asString).standardizedFileURL
     guard let isRegularFile = try? inputFile.resourceValues(forKeys: [.isRegularFileKey]).isRegularFile, isRegularFile == true else {
         throw ArgumentParserError.invalidValue(argument: "--json", error: ArgumentConversionError.custom("File not found"))
     }
 
-    // parse the amazon transcription json file
+    // Parse input file
     var transcript = try AmazonTranscribe.Transcript(file: inputFile)
 
     // Set speaker names
-    if let speakerNames = parsedArguments.get(rawSpeakerNames) {
+    if let speakerNames = parsedArguments.get(speakerNamesArgument) {
         // Counts of names passed on CLI and speakers in Transcript do not match => print warning
         if speakerNames.count != transcript.speakers.count {
             print("Warning: Number of speaker names passed in (\(speakerNames.count)) does not match the number of speakers in the transcript (\(transcript.speakers.count) – \(transcript.speakers.map { $0.speakerLabel }))", to: &stdError)
         }
-        
         for (speakerName, speakerIndex) in zip(speakerNames, transcript.speakers.indices) {
             transcript.speakers[speakerIndex].name = speakerName
         }
@@ -58,15 +57,17 @@ do {
     
     let outputFormat = parsedArguments.get(outputFormatArgument) ?? .default
 
+    // Generate the output text
     let output: String
     switch outputFormat {
     case .markdown:
         output = transcript.makeMarkdown()
-    case .webvtt:
+    case .webVTT:
         output = transcript.makeWebVTT()
     }
 
-    if let outputPath = parsedArguments.get(outputArgument) {
+    // Print output or write to file
+    if let outputPath = parsedArguments.get(outputFileArgument) {
         let url = URL(fileURLWithPath: outputPath.path.asString)
         try Data(output.utf8).write(to: url)
     } else {
@@ -75,5 +76,5 @@ do {
 
 } catch {
     print("Error: \(error)", to: &stdError)
-    exit(-1)
+    exit(1)
 }
